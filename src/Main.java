e69060a1b08979d6e29e9c552d9ffe70cf580e98
Azimxon Toirov <@azimkhon_toirov>
2024-03-14 11:12:32 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Java Calculator!");
+        System.out.println("Enter two numbers:");
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Select operation (+, -, *, /):");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error! Division by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error! Invalid operation.");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+}
